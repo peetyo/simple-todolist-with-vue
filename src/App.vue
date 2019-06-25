6322fec4566b32547fa0f4d6ba82ca2fa4aa29a3
Petar Todorov <petar.bozhidarov.todorov@gmail.com>
2019-06-25 19:13:31 +0200

    Fetching todos from jsonPlaceholder

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,60 @@
 <template>
   <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/> 
   </div>
 </template>
 
 <script>
+import Header from './components/layout/Header'
 import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+import axios from 'axios'
 
 
 export default {
   name: 'app',
   components: {
-    Todos
+    Header,
+    Todos,
+    AddTodo
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
-      ]
+      // todos: [
+      //   {
+      //     id: 1,
+      //     title: "Todo One",
+      //     completed: false
+      //   },
+      //   {
+      //     id: 2,
+      //     title: "Todo Two",
+      //     completed: true
+      //   },
+      //   {
+      //     id: 3,
+      //     title: "Todo Three",
+      //     completed: false
+      //   }
+      // ]
+      todos: []
     }
   },
   methods: {
     deleteTodo(id){
       this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      const {title, completed } = newTodo;
+      this.todos = [...this.todos, newTodo];
     }
+  }, 
+  created(){
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .then(res => this.todos = res.data)
+    .catch(err => console.log(err))
   }
 }
 </script>
@@ -52,5 +69,18 @@ export default {
   body{
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4l
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
   }
 </style>
